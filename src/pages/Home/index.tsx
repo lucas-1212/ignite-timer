054f92759f5cd7a666285f5e2ff538ac8feefff2
lucas-1212 <lucaslp_santos@outlook.com>
2023-01-23 21:00:03 -0300

    feat(timer): finalizado layout do timer

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,55 @@
+import { Play } from "phosphor-react";
+import {
+  FormContainer,
+  HomeContainer,
+  CountdownContainer,
+  Separator,
+  StartCountdownButton,
+  TaskInput,
+  MinutesAmountInput,
+} from "./styles";
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <TaskInput id="task" list="task-suggestions" type="text" placeholder="De um nome para o seu projeto" />
+
+          <datalist id="task-suggestions">
+            <option value="Projeto 1"></option>
+            <option value="Projeto 2"></option>
+            <option value="Projeto 3"></option>
+            <option value="Projeto 4"></option>
+          </datalist>
+
+          <label htmlFor="minutsAmount">Durante</label>
+          <MinutesAmountInput
+            id="minutsAmount"
+            type="number"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+          />
+
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <StartCountdownButton disabled type="submit">
+          <Play />
+          Começar
+        </StartCountdownButton>
+      </form>
+    </HomeContainer>
+  );
+}
